4bf0431d6b643eaffa7846e54961553be72fb6e5
santiago305 <minecratf633@gmail.com>
2025-10-25 09:59:44 -0500

    Update default database connection to pgsql and modify home route to redirect to login

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function __invoke()
+    {
+        return Inertia::render('welcome', [
+            'canRegister' => Features::enabled(Features::registration()),
+        ]);
+    }
+}
